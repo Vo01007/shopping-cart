6e401a73dd7546aa3fc96a5501c66fa8f0b8ff9d
Dmitrii Vasiliu <dimon.v.18@gmail.com>
2024-10-29 21:28:43 +0200

    modified file Products.js

--- src/data/products.js
+++ src/data/products.js
@@ -3,72 +3,84 @@ export const items =[
       "id": 1,
       "title": "Vanilla Cupcakes (6 Pack)",
       "price": 12.99,
-      "image": ".src/images/VanillaCupcakes.jpg"
+      "image": ".src/images/VanillaCupcakes.jpg",
+      "Category": "Cupcake"
     },
     {
       "id": 2,
-      "title": "Frenc hMacaron",
+      "title": "French Macaron",
       "price": 3.99,
-      "image": "./src/images/frenchMacaron.jpg"
+      "image": "./src/images/frenchMacaron.jpg",
+      "Category": "Macaron"
     },
     {
       "id": 3,
       "title": "Pumpkin Cupcake",
       "price": 3.99,
-      "image": "./src/images/pumpkinCupcake.jpg"
+      "image": "./src/images/pumpkinCupcake.jpg",
+      "Category": "Cupcake"
     },
     {
       "id": 4,
       "title": "Chocolate Cupcake",
       "price": 5.99,
-      "image": "./src/images/chocolateCupcake.jpeg"
+      "image": "./src/images/chocolateCupcake.jpeg",
+      "Category": "Cupcake"
     },
     {
       "id": 5,
       "title": "Chocolate Pretzel (4 Pack)",
       "price": 10.99,
-      "image": "./src/images/chocolatePretzel.jpg"
+      "image": "./src/images/chocolatePretzel.jpg",
+      "Category": "Pretzel"
     },
     {
       "id": 6,
       "title": "Strawberry Ice Cream",
       "price": 2.99,
-      "image": "./src/images/strawberryIcecream.jpeg"
+      "image": "./src/images/strawberryIcecream.jpeg",
+      "Category": "Ice cream"
     },
     {
       "id": 7,
       "title": "Chocolate Macarons (4 Pack)",
       "price": 9.99,
-      "image": "./src/images/chocolateMacarons.jpg"
+      "image": "./src/images/chocolateMacarons.jpg",
+      "Category": "Macaron"
     },
     {
       "id": 8,
       "title": "Strawberry Pretzel",
       "price": 4.99,
-      "image": "./src/images/StrawberryPretzel.jpg"
+      "image": "./src/images/StrawberryPretzel.jpg",
+      "Category": "Pretzel"
     },
     {
       "id": 9,
       "title": "Butter Pecan Ice Cream",
       "price": 2.99,
-      "image": "./src/images/ButterPecanIcecream.jpg"
+      "image": "./src/images/ButterPecanIcecream.jpg",
+      "Category": "Ice cream"
     },
     {
       "id": 10,
       "title": "Rocky Road Ice Cream",
       "price": 2.99,
-      "image": "./src/images/rockyRoad.jpg"
+      "image": "./src/images/rockyRoad.jpg",
+      "Category": "Ice cream"
     },
     {
       "id": 11,
       "title": "Vanilla Macaron (5 Pack)",
       "price": 11.99,
-      "image": "./src/images/vanillaMacaron.jpeg"
+      "image": "./src/images/vanillaMacaron.jpeg",
+      "Category": "Macaron"
     },
     {
       "id": 12,
       "title": "Lemon Cupcake (4 Pack)",
       "price": 12.99,
-      "image": "./src/images/lemonCupcake.jpeg"
+      "image": "./src/images/lemonCupcake.jpeg",
+      "Category": "Cupcake"
     }
   ]
